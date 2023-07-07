b908263610142d08cd8ba3a56751a0d108f871d0
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-07-07 18:33:51 +0200

    add-fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addFilter } from 'redux/filterSlice';
-import { selectContacts } from 'redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const [filter, setFilter] = useState('');
 
   const handleInputSearchName = e => {
     const { value } = e.target;
-    setFilter(value);
     dispatch(addFilter(value));
   };
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
 
   return (
     <>
       <h2>Contacts</h2>
       <p>Find contacts by name</p>
-      <input
-        filter={filteredContacts}
-        type="text"
-        name="name"
-        value={filter}
-        onChange={handleInputSearchName}
-      />
+      <input type="text" name="name" onChange={handleInputSearchName} />
     </>
   );
 };
